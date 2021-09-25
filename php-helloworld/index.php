aaa215adb952e801045fcf5647d0a4010dcc6bfd
GTPE Student <vvbui-adlere.fr@bastion.7d30.internal>
2021-09-25 19:39:27 +0000

    Fifth iteration

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,6 +3,7 @@ print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A changed is comming!\n";
 print "Second iteration - A changed is comming!\n";
 echo "Third iteration - A changed is comming!\n";
-echo "fourth iteration - A changed is comming!\n";
+echo "Fourth iteration - A changed is comming!\n";
+echo "Fifth iteration - A changed is comming!\n";
 print "A change is in the air!\n";
 ?>
